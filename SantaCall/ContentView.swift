ffcf2f1d959588706ecb5b8812c80d22e58b43bc
Santiago Sarabia <blvckcenturion@gmail.com>
2025-11-20 18:56:10 -0400

    Initial Commit

---
+++ SantaCall/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SantaCall
+//
+//  Created by sarabiaops on 20/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SantaCall/SantaCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SantaCallApp.swift
+//  SantaCall
+//
+//  Created by sarabiaops on 20/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SantaCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
